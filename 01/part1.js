c2340a1d3458ed164a398df77e5b240300267d21
Luna Winters <luna@gocreate.fun>
2018-12-06 02:15:02 +0000

    Day 1 Part 1 Solution

--- 01/part1.js
+++ 01/part1.js
@@ -1,6 +1,15 @@
 
-const Part1 = () =>{
+import readFileAsync from 'read-file-async'
+const Part1 = async () =>{
+    const buffer = await readFileAsync('./input.txt')
+    const input = buffer.toString()
 
+    const split = input.split('\n')
+    let startFreq = 0
+    for(const sp of split){
+        startFreq += parseInt(sp)
+    }
+    console.log(startFreq)
 }
 
 export default Part1
