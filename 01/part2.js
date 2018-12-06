8e1632a8bb1fff729e23c8fcb6e7e765200c2347
Luna Winters <luna@gocreate.fun>
2018-12-06 02:25:21 +0000

    Day 1 part 2 solution

--- 01/part2.js
+++ 01/part2.js
@@ -1,6 +1,24 @@
+import readFileAsync from 'read-file-async'
+const Part2 = async () =>{
+    const buffer = await readFileAsync('./input.txt')
+    const input = buffer.toString()
 
-const Part2 = () =>{
-
+    const split = input.split('\n')
+    let freq = 0
+    const hadFrequencies = [freq] //Frequencies already processed
+    let looping = true
+    let loopCount = 0;
+    while(looping){
+        for(const sp of split){
+            freq += parseInt(sp)
+            if(hadFrequencies.includes(freq)){
+                console.log('Part 2: '+freq)
+                looping = false
+                break;
+            }
+            hadFrequencies.push(freq)
+        }
+    }
 }
 
 export default Part2
