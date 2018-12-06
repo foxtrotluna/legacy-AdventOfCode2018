fa42354fc208e795b117bffbb697ba48c04cb185
Luna Winters <luna@gocreate.fun>
2018-12-06 02:39:05 +0000

    Day 2 part 1

--- 02/part1.js
+++ 02/part1.js
@@ -1,6 +1,41 @@
+import readFileAsync from 'read-file-async'
+const Part1 = async () =>{
+    const buffer = await readFileAsync('./input.txt')
+    const input = buffer.toString()
+    let doublesCounter = 0 //Boxes with 2 duplicate letters
+    let triplesCounter = 0 //Boxes with 3 duplicate letters
+    for(const line of input.split('\n')){
+        if(hasLetterCount(line,2)){
+            doublesCounter++
+        }
+        if(hasLetterCount(line,3)){
+            triplesCounter++
+        }
+    }
+    console.log(doublesCounter*triplesCounter)
+}
 
-const Part1 = () =>{
+const hasLetterCount = (str,c) => {
+    const counts = getCharCounts(str)
+    for(const letter of Object.keys(counts)){
+        if(counts[letter] === c){
+            return true
+        }   
+    }
+    return false
+}
 
+const getCharCounts = str =>{
+    let output = {}
+    for(const ch of str){
+        if(output[ch]){
+            output[ch]++
+        }
+        else{
+            output[ch] = 1
+        }
+    }
+    return output
 }
 
 export default Part1
--- 02/part2.js
+++ 02/part2.js
@@ -1,5 +1,7 @@
-
-const Part2 = () =>{
+import readFileAsync from 'read-file-async'
+const Part2 = async () =>{
+    const buffer = await readFileAsync('./input.txt')
+    const input = buffer.toString()
 
 }
 
